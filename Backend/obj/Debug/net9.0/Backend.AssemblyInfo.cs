609d41636235f0d4148be86a110f99528e187dca
David <david.markovic@elfak.rs>
2026-02-06 13:57:14 +0100

    feat: add BCrypt and JWT Bearer authentication packages; remove unused UserMapper

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+845d06954d7d695eb5d4850eb11bae851e1842c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ebc9cfc49fb491f2582a120680d8ba86647a57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
